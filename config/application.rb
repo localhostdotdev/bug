23270cffeef0c5de791a7f80032844ce54da961c
localhostdotdev <localhostdotdev@protonmail.com>
2019-05-08 14:56:25 +0200

    very minimal rails app

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,10 @@ Bundler.require(*Rails.groups)
 module Bug
   class Application < Rails::Application
     config.load_defaults 6.0
+
     config.eager_load = false
-    config.generators.test_framework = nil
     config.action_dispatch.cookies_serializer = :json
+
+    config.generators.test_framework = nil
   end
 end
