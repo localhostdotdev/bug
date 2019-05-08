ed8d228334393ba2ec4c027c282cae67af422554
localhostdotdev <localhostdotdev@protonmail.com>
2019-05-08 14:47:25 +0200

    remove most config

--- config/application.rb
+++ config/application.rb
@@ -7,14 +7,7 @@ Bundler.require(*Rails.groups)
 module Bug
   class Application < Rails::Application
     config.load_defaults 6.0
-    config.generators.system_tests = nil
-
-    config.assets.version = '1.0'
+    config.generators.test_framework = nil
     config.action_dispatch.cookies_serializer = :json
-    config.filter_parameters += [:password]
-
-    ActiveSupport.on_load(:action_controller) do
-      wrap_parameters format: [:json]
-    end
   end
 end
